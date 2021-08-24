49354c4074c9ba4ec577de5ca02898612f973db2
m-nagashio <m.nagashio.n618m@gmail.com>
2021-08-25 08:50:01 +0900

    Referctering

--- convert_heic2png.py
+++ convert_heic2png.py
@@ -1,20 +1,19 @@
 from PIL import Image
 import pyheif
-
-def conv(image_path):
-    new_name = image_path.replace('HEIC', 'png')
-    heif_file = pyheif.read(image_path)
-    data = Image.frombytes(
-        heif_file.mode,
-        heif_file.size,
-        heif_file.data,
-        "raw",
-        heif_file.mode,
-        heif_file.stride,
-        )
-    data.save(new_name, "png")
-
 import glob
-lst = glob.glob("*.HEIC")
-for l in lst:
-    conv(l)
+
+def convert(file_name):
+    heif = pyheif.read(file_name)
+    data = Image.frombytes(
+        heif.mode,
+        heif.size,
+        heif.data,
+        "raw",
+        heif.mode,
+        heif.stride,
+        )
+    data.save(file_name.replace('HEIC', 'png'), "png")
+
+file_names = glob.glob("*.HEIC")
+for f in file_names:
+    convert(f)
